95c20d01c649cfdddbba7b2ccb37899d9df88a1f
rjmacarthy <richardmacarthy@hotmail.com>
2018-05-08 21:52:45 +0100

    Build interface

--- interfaces/Ownable_sol_Ownable.ts
+++ interfaces/Ownable_sol_Ownable.ts
@@ -1,8 +1,8 @@
 
     export interface IBaseMethods {
         call: (options?: any, callback?: Function) => Promise<any>,
-        send: (options?: any, callback?: Function) => Promise<any>,
+        send: (options: any, callback?: Function) => Promise<any>,
         estimateGas: (options?: any, callback?: Function) => Promise<any>,
-        encodeABI: (options?: any, callback?: Function) => Promise<any>
+        encodeABI: () => Promise<any>
     }
     export interface IOwnable_sol_Ownable { counter : () => IBaseMethods;_name : () => IBaseMethods;}
